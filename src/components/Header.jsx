87d39b8ad20bad2efed9747eb6fe7e0070c3f944
shibaramdas-git <shibaram.daseng@gmail.com>
2024-12-17 10:59:06 +0530

    Header finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,19 @@
-import React from "react";
-
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="c_pad py-[24px] md:py-[30px] w-full font-cheaps border border-black flex justify-between items-center">
+      <div className="menu p-[2px]">
+        <span className="w-8 h-[3px] block mb-[12px] bg-black rounded-lg"></span>
+        <span className="w-10 h-[3px] block mb-[12px] bg-black  rounded-lg"></span>
+        <span className="w-6 h-[3px] block bg-black rounded-lg"></span>
+      </div>
+      <div className="logo flex items-center gap-1 md:gap-2">
+        <img src="./logo.png" alt="logo" />
+        <div className="font-30">
+          DESIGN <br />
+          HUB
+        </div>
+      </div>
+      <div className="font-46 self-end hidden md:block">HOME</div>
+    </header>
+  );
 }
