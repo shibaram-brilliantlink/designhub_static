08f05f723b6550bc1a9cfa283a66e0d7bf5dbb34
shibaramdas-git <shibaram.daseng@gmail.com>
2024-12-18 12:40:32 +0530

    Footer finshed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer>
-      <div className="top border border-black flex justify-between items-center c_pad">
-        <div className="inline-flex flex-wrap gap-2 w-[80%] items-start md:flex-row ">
+    <footer className="c_pad pt-[80px] mb-[2rem] md:pt-[100px] md:mb-[3.4375rem]">
+      <div className="top flex justify-between items-center mb-[60px] lg:mb-[80px]">
+        <div className="inline-flex flex-wrap gap-x-3 gap-y-3 w-[80%] items-start md:flex-row">
           <a href="#" className="btn-footer">
             HOME
           </a>
@@ -18,13 +18,13 @@ export default function Footer() {
             ABOUT
           </a>
         </div>
-        <div className="circular-red p-6">
+        <div className="circular-red p-7 text-3xl lg:text-3xl lg:p-10 ">
           <span>&uarr;</span>
         </div>
       </div>
-      <div className="center flex justify-between">
-        <div className="left">
-          <h4 className="font-cheaps font-55">DESIGN HUB CO. LTD</h4>
+      <div className="center flex flex-col md:flex-row justify-between mb-4">
+        <div className="left mb-6 font-30 text-neutral-700">
+          <h4 className="font-cheaps font-55 text-black">DESIGN HUB CO. LTD</h4>
           <p>
             Jakob-Funke-Platz 2 <br />
             45127 Essen <br />
@@ -32,19 +32,26 @@ export default function Footer() {
               contactus@designhub.com
             </a>
           </p>
-          <p>Terms or use | Privacy Policy</p>
+          <p className="text-lg mt-8 md:mt-[80px] hidden md:block">
+            Terms or use | Privacy Policy
+          </p>
         </div>
-        <ul className="right font-cheaps text-2xl">
+        <ul className="right font-cheaps mb-6 font-46 md:text-right">
           <li>INSTAGRAM</li>
           <li>FACEBOOK</li>
           <li>LINKEDIN</li>
           <li>DRIBBLE</li>
           <li>BEHANCE</li>
         </ul>
+        <p className="text-lg md:hidden">Terms or use | Privacy Policy</p>
       </div>
-      <div className="bottom">
-        <div className="footer-font">TALKTOUS</div>
-        <div className="footer-btn">BOOK A CALL NOW</div>
+      <div className="bottom flex justify-center flex-wrap gap-x-4 lg:gap-x-6 sm:flex-nowrap">
+        <div className="footer-font font-cheaps h-[18vw] md:translate-y-[14%] mb-4 md:mb-0">
+          TALKTOUS
+        </div>
+        <div className="footer-book-btn font-cheaps w-full sm:w-auto flex-1 flex items-center justify-center">
+          BOOK A <br className="hidden sm:block" /> CALL NOW
+        </div>
       </div>
     </footer>
   );
