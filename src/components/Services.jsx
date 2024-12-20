764cbf7e1ec47a2060568dddd4b5f924afbc9518
shibaramdas-git <shibaram.daseng@gmail.com>
2024-12-20 15:08:41 +0530

    Testimonial section DESIGN finished , swapping pending.

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -10,25 +10,25 @@ export default function Services() {
         </div>
       </div>
       <div>
-        <div className="flex justify-between items-center border-t-2 border-gray-700 py-3 lg:py-[40px] hover:text-white hover:bg-orange group hover:scale-y-[105%] hover:border-none duration-200 cursor-pointer">
+        <div className="flex justify-between items-center border-t-2 border-gray-700 py-3 lg:py-[40px] hover:text-white hover:border-orange hover:bg-orange group duration-200 cursor-pointer">
           <div className="font-55 pl-[2%]">BRANDING</div>
           <div className="circular-black p-5 md:p-8 mr-[2%] group-hover:border-cream">
             <span>&#8702;</span>
           </div>
         </div>
-        <div className="flex justify-between items-center border-t-2 border-gray-700 py-3 lg:py-[40px] hover:text-white hover:bg-orange group hover:scale-y-[105%] hover:border-none duration-200 cursor-pointer">
+        <div className="flex justify-between items-center border-t-2 border-gray-700 py-3 lg:py-[40px] hover:text-white hover:bg-orange group hover:border-orange duration-200 cursor-pointer">
           <div className="font-55 pl-[2%]">UI/UX DESIGN</div>
           <div className="circular-black p-5 md:p-8 mr-[2%] group-hover:border-cream">
             <span>&#8702;</span>
           </div>
         </div>
-        <div className="flex justify-between items-center border-t-2 border-gray-700 py-3 lg:py-[40px] hover:text-white hover:bg-orange group hover:scale-y-[105%] hover:border-none duration-200 cursor-pointer">
+        <div className="flex justify-between items-center border-t-2 border-gray-700 py-3 lg:py-[40px] hover:text-white hover:bg-orange group hover:border-orange duration-200 cursor-pointer">
           <div className="font-55 pl-[2%]">ILLUSTRATION</div>
           <div className="circular-black p-5 md:p-8 mr-[2%] group-hover:border-cream">
             <span>&#8702;</span>
           </div>
         </div>
-        <div className="flex justify-between items-center border-y-2 border-gray-700 py-3 lg:py-[40px] hover:text-white hover:bg-orange group hover:scale-y-[105%] hover:border-none duration-200 cursor-pointer">
+        <div className="flex justify-between items-center border-y-2 border-gray-700 py-3 lg:py-[40px] hover:text-white hover:bg-orange group hover:border-orange duration-200 cursor-pointer">
           <div className="font-55 pl-[2%]">3D/ANIMATION</div>
           <div className="circular-black p-5 md:p-8 mr-[2%] group-hover:border-cream">
             <span>&#8702;</span>
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -2,30 +2,34 @@ import React from "react";
 
 export default function Testimonial() {
   return (
-    <section className="c_pad py-[40px] lg:py-[80px] bg-skin">
+    <section className="c_pad py-[40px] lg:py-[80px] testimonial-bg">
       <h2 className="font-cheaps font-110 mb-[30px] md:mb-[60px]">
         HOW CLIENTS FEEL
       </h2>
-      <div className="flex justify-between ">
-        <div className="left hidden md:flex flex-col">
-          <div>
-            <img src="./reviews-01.png" alt="person01" />
+      <div className="flex justify-between">
+        <div className="left hidden md:block relative pt-10">
+          <div className="pl-5 lg:pl-10">
+            <img src="./reviews-01.png" alt="person01" width={150} />
           </div>
-          <div className="circular-red p-6">
+          <div className="circular-red p-10 bottom-0 bg-skin">
             <span>&#8701;</span>
           </div>
         </div>
-        <div className="review-card p-10 bg-white rounded-md flex flex-col items-center gap-4 md:w-[50%] max-w-[600px]">
+        <div className="review-card p-10 bg-white rounded-md flex flex-col items-center gap-4 md:w-[50%] max-w-[600px] mx-auto">
           <img
             src="./reviews-02.png"
             alt="iamge"
-            width={120}
-            className="rounded-md"
+            width={150}
+            className="rounded-md mb-4"
           />
           <div className="font-cheaps font-55">JANE COPPER</div>
-          <img src="./reviews-comma.png" alt="review image of comma" />
+          <img
+            src="./reviews-comma.png"
+            alt="review image of comma"
+            width="50"
+          />
           <p className="text-sm text-center">
-            Working with desing hub was an absolute pleasure! Their attention to
+            Working with design hub was an absolute pleasure! Their attention to
             detail, creativity and commitemnet to delivering a high-quality
             product excedded my expectations. The communication was seamless,
             and they went above and beyond to ensure everything was perfect. I
@@ -33,11 +37,11 @@ export default function Testimonial() {
             services to anyone looking for top-notch services.
           </p>
         </div>
-        <div className="right hidden md:flex flex-col items-end">
-          <div>
-            <img src="./reviews-03.png" alt="person01" />
+        <div className="right hidden md:block pt-10 relative">
+          <div className="pr-5 lg:pr-10">
+            <img src="./reviews-03.png" alt="person03" width={150} />
           </div>
-          <div className="circular-red p-6 ">
+          <div className="circular-red p-10 bottom-0 right-0 bg-skin">
             <span>&#8702;</span>
           </div>
         </div>
